c6f4f78f66d0252a6eaf64250ebaebc6b6f0a3c1
MELVERTON\Mel <huntermelverton@gmail.com>
2018-11-10 23:33:58 -0500

    bonding graphql with component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { Query, graphql } from 'react-apollo';
 
 class SongList extends Component {
   render() {
+    let {loading, songs} = this.props.data;
+    console.log(this.props)
+    if (loading) return null
     return (
-      <div>Hi</div>
+      <div>
+        {songs.map(song => (
+          <div>{song.title}</div>
+        ))}
+      </div>
     )
   }
 }
 
-export default SongList;
+const query = gql `
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
